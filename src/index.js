c811f895166aa78081011527028162881e409e1a
Masood Ahmad <masahmad7@gmail.com>
2018-01-08 16:40:49 +0000

    initial commit of dp-table-builder-ui

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = ' dp-table-builder-ui  boilerplate - ready...';
+
+ReactDOM.render(
+  <div><h1>{title}</h1></div>,
+  document.getElementById('app')
+);
+var x=1
+cxcx
+module.hot.accept();

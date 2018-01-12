212391adb2ee992dc1e4a26e67c70e4617829cbf
Masood Ahmad <masahmad7@gmail.com>
2018-01-12 11:38:49 +0000

    gridcomponent update build html json

--- src/components/gridComponent.jsx
+++ src/components/gridComponent.jsx
@@ -17,22 +17,173 @@ class GridComponent extends React.Component {
   }
 
 
+
+
   handleChange(event) {
     const targetId = event.target.id;
+
+  
     console.log(hot);
-    console.log(event);
+    var x = hot.innerHTML;
+    var table = hot.getElementsByTagName('table')[0];
+    console.log(table);
+    console.log(typeof(table));
+
+
+    //console.log(this.refs.hot.hotInstance.getData());
+   // console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+
+    
+
+    //init
+    var tableJsonOutput = {'filename':'abc1234'};
+    var cell_formats = [];
+    var column_formats =[];
+
+    for (var r = 0, row; row = table.rows[r]; r++) {
+      //iterate through rows
+      // console.log('@row=' + i);
+      //rows would be accessed using the "row" variable assigned in the for loop
+      for (var c = 0, col; col = row.cells[c]; c++) {
+        //iterate through columns
+        //if col.outerHTML
+        var alignClass = col.getAttribute('class');
+        var rowSpan = col.getAttribute('rowSpan');
+        var colSpan = col.getAttribute('colSpan');
+        if (alignClass != '' || rowSpan!=undefined || colSpan!=undefined) {
+          console.log('@row =' + r + ' @col=' + c + '  cls=' + alignClass + ' rowSpan=' + rowSpan + ' colSpan=' + colSpan);
+          console.log('...HTML:  ' + col.outerHTML);
+          cell_formats.push(this.getCellFormat(r, c, alignClass, rowSpan, colSpan))
+
+        }
+      }
+    }
+
+
+    // build json
+    tableJsonOutput['cell_formats']= cell_formats;
+    tableJsonOutput['data']= this.refs.hot.hotInstance.getData();
+    tableJsonOutput['handsontable']={"table": table.outerHTML};
+
+
+    console.log('-------------------');
+    console.log(tableJsonOutput)
+    //console.log(JSON.stringify(tableJsonOutput));
+
   }
 
+
+
+  getCellFormat(rowNum, colNum, alignType, rspan, cspan) {
+    var valign, halign;
+
+    switch (alignType) {
+      case 'htLeft':
+        halign = 'left';
+        break;
+      case 'htRight':
+        halign = 'right';
+        break;
+      case 'htCenter':
+        halign = 'center';
+        break;
+      case 'htJustify':
+        halign = 'justify';
+        break;
+
+
+      case 'htTop':
+        valign = 'top';
+        break;
+      case 'htMiddle':
+        valign = 'middle';
+        break;
+      case 'htBottom':
+        valign = 'bottom';
+        break;
+
+      //left combination
+      case 'htLeft htTop':
+        halign = 'left';
+        valign = 'top';
+        break;
+      case 'htLeft htMiddle':
+        halign = 'left';
+        valign = 'middle';
+        break;
+      case 'htLeft htBottom':
+        halign = 'left';
+        valign = 'bottom';
+        break;
+
+      //right combination
+      case 'htRight htTop':
+        halign = 'right';
+        valign = 'top';
+        break;
+      case 'htRight htMiddle':
+        halign = 'right';
+        valign = 'middle';
+        break;
+      case 'htRight htBottom':
+        halign = 'right';
+        valign = 'bottom';
+        break;
+
+      //center combination
+      case 'htCenter htTop':
+        halign = 'center';
+        valign = 'top';
+        break;
+      case 'htCenter htMiddle':
+        halign = 'center';
+        valign = 'middle';
+        break;
+      case 'htCenter htBottom':
+        halign = 'center';
+        valign = 'bottom';
+        break;
+
+      //justify combination
+      case 'htJustify htTop':
+        halign = 'justify';
+        valign = 'top';
+        break;
+      case 'htJustify htMiddle':
+        halign = 'justify';
+        valign = 'middle';
+        break;
+      case 'htJustify htBottom':
+        halign = 'justify';
+        valign = 'bottom';
+        break;
+
+      default:
+
+    }
+    console.log('in method row (r) is' + rowNum)
+    var cellObj = { "row": rowNum, "col": colNum, "align": halign, "vertical_align": valign, "rowspan": rspan, "colspan": cspan }
+    return cellObj;
+  }
+
+
+  componentDidMount() {
+    console.log('@component did mount');
+  }
 
   render() {
     return <div className='gridContainer'>
       <HotTable
-      root="hot"
+        root="hot"
         data={this.handsontableData}
         contextMenu={true}
         colHeaders={true}
+        mergeCells={true}
+        manualColumnResize={true}
+        manualRowResize={true}
+        ref="hot"
       />
-      <button  onClick={(e) => this.handleChange(e)}>test</button>
+      <button onClick={(e) => this.handleChange(e)}>test</button>
     </div>;
 
   }

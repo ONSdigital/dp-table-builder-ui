68867f14801404f0a3a640a6e8b5368e7f4477e1
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-02-07 15:15:35 +0000

    [MS] added a test-app

---
+++ test-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import GridContainer from '../../dist';
+
+
+ReactDOM.render(
+<div id="example-component">
+<GridContainer />
+</div>, 
+document.getElementById('app'));

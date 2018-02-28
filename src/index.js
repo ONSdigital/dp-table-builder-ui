851c3b96867a1adcd72316478752289246fbccca
Matthew Smith <matthew.smith@thinkinglogic.com>
2018-02-28 12:31:23 +0000

    [MS] Removed prepublishOnly script form package.json, reverted changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
-// import GridContainer from './components/gridContainer';
-
-// import  "./assets/scss/main.scss";
-
-// export default GridContainer;
-
-
-
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import './index.css';
 import GridContainer from './components/gridContainer';
 
+import  "./assets/scss/main.scss";
 
-
-
-
-const onError = function(msg) {
-    alert(msg);
-}
-
-function onSave(result) {
-    console.log("onSave invoked. result=" + JSON.stringify(result));
-}
-function onCancel() {
-    console.log("onCancel invoked.");
-}
-
-
-
-ReactDOM.render(
-    <div id="example-component">
-        <GridContainer onCancel={onCancel} onSave={onSave} onError={onError} data={{}} />
-    </div>, 
-    document.getElementById('app'));
-
+export default GridContainer;

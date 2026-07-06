4ec8df0488ed8160f2f90d4e2fab8a5a957e4e69
Ighomena Odebala <ighomena.odebala@ons.gov.uk>
2026-07-06 10:42:06 +0100

    Update test configs

--- config/jest/shim.js
+++ config/jest/shim.js
@@ -3,3 +3,14 @@
 global.requestAnimationFrame = function(callback) {
   setTimeout(callback, 0);
 };
+
+// TextEncoder/TextDecoder are not exposed globally in jest-environment-jsdom 27.
+const { TextEncoder, TextDecoder } = require('util');
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+
+// Web Streams API globals required by undici (via cheerio/enzyme) on Node 18+.
+const { ReadableStream, WritableStream, TransformStream } = require('node:stream/web');
+global.ReadableStream = ReadableStream;
+global.WritableStream = WritableStream;
+global.TransformStream = TransformStream;
